3ecaa24c41b368314aa7b3e7be6800e28436e02f
ShahadathAlam <shahadathalam@ymail.com>
2024-10-08 23:32:12 +0600

    Creating AccountSlice using RTK by the createSlice function and also used prepare when you need your action creator to receive more than one argument

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -22,7 +22,8 @@ function AccountOperations() {
   console.log(balance);
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount, currency));
+    // dispatch(deposit(depositAmount, currency));
+    dispatch(deposit(depositAmount));
     setDepositAmount("");
     setCurrency("USD");
   }
